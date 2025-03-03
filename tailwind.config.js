38bec0cafbf84e60ae0299ac622b58321f3b8ff1
pradeep-kalyan <pradeepkalyan1275@gmail.com>
2025-03-03 22:55:49 +0530

    solved the bug issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
