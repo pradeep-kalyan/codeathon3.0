8778cfadc680fec717df0ca7769bbde0fe5ac62a
pradeep-kalyan <pradeepkalyan1275@gmail.com>
2025-03-03 22:44:38 +0530

    solved the bug issues

--- src/components/InstagramEmbed.jsx
+++ src/components/InstagramEmbed.jsx
@@ -21,7 +21,7 @@ const InstagramEmbed = ({ url, text }) => {
   return (
     <div className="flex flex-col p-3 justify-center items-center transition-all duration-300 transform hover:scale-105 hover:shadow-red-500 hover:shadow-lg">
       <blockquote
-        className="flex justify-center custom-cursor items-center border-4 border-red-500 instagram-media"
+        className="flex justify-center cursor-pointer items-center border-4 border-red-500 instagram-media"
         data-instgrm-permalink={url}
         data-instgrm-version="14"
         data-instgrm-media-type="video"
